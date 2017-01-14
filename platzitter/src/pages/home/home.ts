5bc5f6361f765f210cec230bede0cec9f0f5b348
jorgeucano <jorgeucano@gmail.com>
2017-01-14 00:28:39 -0300

    platzitter ya arranco

---
+++ platzitter/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+    listado:Array<twitt> = [
+      {img:'https://pbs.twimg.com/profile_images/635135056336523264/Zoe8kLQX_400x400.jpg', nick_name:'@freddier', text:'platzitter es lo mas'},
+      {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+    ];
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  
+
+
+}
+
+interface twitt{
+  img:string;
+  nick_name:string;
+  text:string;
+}
