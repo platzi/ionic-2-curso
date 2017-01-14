8d876916a4b5030b990226ac492ef4463aadfe68
jorgeucano <jorgeucano@gmail.com>
2017-01-14 09:05:12 -0300

    vista de profile

--- platzitter/src/pages/contact/contact.ts
+++ platzitter/src/pages/contact/contact.ts
@@ -8,8 +8,50 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  me:Me = {
+    "img":'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg',
+     nick_name:'@jorgeucano',
+    "interactions":1,
+    "github": "jorgeucano",
+    "twitter": "jorgeucano",
+    "medium": "jorgeucano",
+    "bio": "Jorge Cano es Google Developer Expert en Angular y Tecnologías Web. Organizador de ngbaires. Director de tecnologias en JavaScript es especialista en Angular y Firebase. Tambien da cursos, charlas y escribe articulos tecnicos."
+  };
 
+
+  listado:Array<twitt> = [
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+      {img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+    ];
+
+    
+
+
+  constructor(public navCtrl: NavController) {
+    this.me.interactions = this.listado.length;
   }
 
+}
+
+
+interface Me{
+  img:string;
+  nick_name:string;
+  interactions:number;
+  github:string;
+  twitter:string;
+  medium:string;
+  bio:string;
+}
+
+interface twitt{
+  img:string;
+  nick_name:string;
+  text:string;
 }
