67774d598e1e0f3beac2a36873accb6ea5d261fd
jorgeucano <jorgeucano@gmail.com>
2017-01-14 08:38:13 -0300

    UI Notificaciones

--- platzitter/src/pages/about/about.ts
+++ platzitter/src/pages/about/about.ts
@@ -8,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+
+  notifications:Array<Notifications> = [
+      {action: 'reply', img:'https://pbs.twimg.com/profile_images/635135056336523264/Zoe8kLQX_400x400.jpg', nick_name:'@freddier', text:'platzitter es lo mas'},
+      {action: 'replatzitt', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+      {action: 'mention', img:'https://pbs.twimg.com/profile_images/807699331633598464/A_9pIQ6d.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+    ];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface Notifications{
+  img:string;
+  nick_name:string;
+  text:string;
+  action:string;
 }
